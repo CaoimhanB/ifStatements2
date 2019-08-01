f9985db9030bbae4a7dbf9158705e5a027ec5ea5
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-08-01 10:21:05 +0100

    Add project files.

---
+++ ifStatements2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ifStatements2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(200, 100, 50));
+
+
+
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3 ){
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+            return result;
+
+            
+        } 
+    }
+}
